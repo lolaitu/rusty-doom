86f131830a116c80a60e263a294e8f08f1c3be46
moua <moua@moua-Latitude-5480-of-lolaitu@linux-casser.fr>
2025-02-05 17:00:37 +0100

    bidouille

---
+++ src/editor.rs
@@ -0,0 +1,10 @@
+
+
+fn main () {
+    println!("{}", clear::All);
+	cursor::goto(1,1)
+	print!("you are in the editor");
+	cursor::goto(1,2)
+	print!("press q to quit : ");
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,28 @@
-extern crate termion;
-
-use std::{thread, time};
-use termion::{clear, color, cursor, terminal_size};
-use termion::raw::IntoRawMode;
-use std::io::{Write, stdout};
-
-
-fn fragment(uv: [f32; 2], time: f32) -> [f32; 3] {
-    [uv[1], uv[0], time % 1.]
-}
+use std::io::{stdout, Stdout, Write};
+use std::time::Duration;
+use crossterm;
 
 fn main() {
-    println!("{}", clear::All);
-    let mut timer: f32 = 0.;
+    let mut stdout: Stdout = stdout();
 
-    // let mut stdout = stdout().into_raw_mode().unwrap();
-    // write!(stdout, "Hey there.").unwrap();
+    // Activer le mode brut du terminal
+    terminal::enable_raw_mode();
+    stdout.execute(EnterAlternateScreen);
+    stdout.execute(Hide);
 
     loop {
-        let mut screen_str = String::new();
-
-        let term_size = match terminal_size() {
-            Ok((width, height)) => (width as f32, height as f32),
-            Err(_) => {
-                eprintln!("Failed to get terminal size");
-                return;
-            }
-        };
-
-        for i in 0..term_size.0 as u16 {
-            for j in 0..term_size.1 as u16 {
-                let uv = [i as f32 / term_size.0, j as f32 / term_size.1];
-                let color = fragment(uv, timer);
-
-                screen_str.push_str(&format!(
-                    "{}{} ",
-                    cursor::Goto(i + 1, j + 1),
-                    color::Bg(color::Rgb(
-                        (color[0] * 255.0) as u8,
-                        (color[1] * 255.0) as u8,
-                        (color[2] * 255.0) as u8
-                    ))
-                ));
+        if poll(Duration::from_millis(500)) {
+            if let Event::Key(event) = event::read() {
+                if event.code == KeyCode::Char('q') {
+                    break;
+                }
             }
         }
-    print!("{}", screen_str);
-
-    thread::sleep(time::Duration::from_millis(100));
-    timer += 0.1;
+        stdout.flush();
     }
 
+    // Désactivation propre du mode brut
+    stdout.execute(Show);
+    stdout.execute(LeaveAlternateScreen);
+    terminal::disable_raw_mode();
 }
