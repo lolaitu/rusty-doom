34c13b08716f0ac2311c5002018c3f412001bd72
Noah Raimbaud <raimbaudnoah@gmail.com>
2025-02-17 11:23:11 +0100

    il push enfin ce connard

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,61 @@
-use std::io::Result;
+use device_query::{DeviceQuery, DeviceState, MouseState, Keycode}; // To detect keyboard and mouse position
 use crossterm::{
+    event::{self, Event, KeyCode},
     execute,
-    cursor::{Hide, Show},
-    terminal::{self,
-        EnterAlternateScreen,
-        LeaveAlternateScreen
-    },
+    terminal::{self, ClearType},
+    cursor::MoveTo,
 };
 
-mod game;
-use game::Game;
+mod player;  // Importer le fichier player.rs
+use player::Player;  // Importer la struct Player
 
-fn main() -> Result<()> {
+mod mainGame;
+use mainGame::MainGame;
 
-    let mut write = std::io::stdout();
+use enigo::{Enigo, MouseControllable}; // To move the mouse
+use std::io::{stdout, Write};
+use std::io::Result;
+use std::thread::sleep;
+use std::time::Duration;
+use rand::Rng; // Add this to your imports
 
-    terminal::enable_raw_mode()?;
-    execute!(write,
-        EnterAlternateScreen,
-        Hide
-    )?;
 
-    let mut my_game = Game::new();
-    my_game.launch()?;
+fn main() -> Result<()>{
+    let mut maze = Vec::new();
+    let mut rng = rand::thread_rng();
 
-    execute!(write,
-        LeaveAlternateScreen,
-        Show
-    )?;
-    terminal::disable_raw_mode()?;
+    for y in 0..40 { 
+        if y == 0 || y == 39 {
+            // Bord supérieur et inférieur rempli de #
+            maze.push(vec!['#'; 160]);
+        } else {
+            // Bordures gauche et droite avec des espaces au milieu et des # aléatoires
+            let mut line = vec!['#'];
+            for x in 1..159 {
+                // 5% chance to place a wall
+                //if rng.gen_ratio(1, 20) {
+                //    line.push('#');
+                //} else {
+                    
+                //}
+                if (y == 20 && x<100 && x>50) || (x == 50 && y>20 && y<30){
+                    line.push('#');
+                }
+                else{
+                    line.push(' ');
+                }
+            }
+            line.push('#');
+            maze.push(line);
+        }
+    }
 
+    let MAZE: &Vec<Vec<char>> = &maze;
+
+    let mut mainGame = MainGame::new(MAZE);
+    mainGame.init()?;
+    
+    
     Ok(())
 }
+
